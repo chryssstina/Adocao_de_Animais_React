4037df1efffbb6c81685e0692e209f0c218027bb
Luiz Carlos <lcarlos.mln@gmail.com>
2025-07-03 18:45:13 -0300

    Refactor Footer and Navbar components for improved readability and structure

---
+++ Latidos_E_Ronrons/src/components/MainSection/index.jsx
@@ -0,0 +1,16 @@
+
+function MainSection() {
+  return (
+    <section className="home-section">
+      <div className="home-section-text">
+        <h1> Adotar é um ato de amor </h1>
+        <h2> Conheça nossos amigos de quatro patas que estão em busca de um lar amoroso. </h2>
+        <button type="button" className="btn btn-lg btn-adotar"> <a href="#form-section"> Quero adotar </a> </button>
+
+      </div>
+      <img className="home-section-img mt-3 me-3" src="./assets/Miscellaneous/Images.png" alt="Logo Latidos&Ronrons" />
+    </section>
+  );
+}
+
+export default MainSection;
