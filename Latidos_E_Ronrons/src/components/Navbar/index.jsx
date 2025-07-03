6cf8d9651346bffed601bee977ad7b44a5a2d94a
Luiz Carlos <lcarlos.mln@gmail.com>
2025-07-03 18:07:11 -0300

    Redesign Navbar and update global styles
    Refactored the Navbar component to use a new layout with a larger logo, improved navigation links, and responsive design. Overhauled index.css to introduce a new color palette, modernized layout, and improved section styling for better consistency and responsiveness across the site.

--- Latidos_E_Ronrons/src/components/Navbar/index.jsx
+++ Latidos_E_Ronrons/src/components/Navbar/index.jsx
@@ -3,15 +3,30 @@ import Logo from "../Logo";
 
 function Navbar() {
   return (
-      <nav id="navbar" className="navbar navbar-expand-lg sticky-top">
-        <div className="container-fluid d-flex">
-          <Logo />
-          <div className="justify-content-start">
-          <a className="navbar-brand" href="#" id="navbarTitle">Latidos & Ronrons</a>
+    <>
+      <div className="header-container">
+        <nav className="navbar navbar-expand-lg">
+          <div className="container-fluid">
+            <img className="navbar-img logo-grande" src="imgs/LogoTitulo/LogoTitle2.png" alt="Logo Latidos&Ronrons" />
+            {/* <img className="navbar-img logo-pequena" src="imgs/Logos/logo3.png" alt="Logo Latidos&Ronrons" /> */}
+
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav ms-auto">
+                <a className="nav-link" href="#about">Sobre</a>
+                <a className="nav-link" href="#programming-section">Programação</a>
+                <a className="nav-link" href="#form-section">Inscrição</a>
+                <a className="nav-link" href="#photo-gallery-section">Galeria</a>
+              </div>
+            </div>
           </div>
-          <Links />
-        </div>
-      </nav>
+        </nav>
+      </div>
+
+    </>
   );
 }
 
