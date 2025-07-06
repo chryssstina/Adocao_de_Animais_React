e24c0a978a6c9f2954b5e6712491b038cfb01ba9
Chrystina <chrystinamaria3@gmail.com>
2025-07-06 00:25:35 -0300

    Adicionando páginas de Login e SignUp

--- Latidos_E_Ronrons/src/components/MainSection/index.jsx
+++ Latidos_E_Ronrons/src/components/MainSection/index.jsx
@@ -1,4 +1,5 @@
 import Animals from '../../assets/Miscellaneous/animals.png/';
+import BtnQueroAdotar from '../BtnQueroAdotar/BtnQueroAdotar';
 
 function MainSection() {
   return (
@@ -6,8 +7,7 @@ function MainSection() {
       <div className="home-section-text">
         <h1> Adotar é um ato de amor </h1>
         <h2> Conheça nossos amigos de quatro patas que estão em busca de um lar amoroso. </h2>
-        <button type="button" className="btn btn-lg btn-adotar"> <a href="#form-section"> Quero adotar </a> </button>
-
+        <BtnQueroAdotar/>
       </div>
       <img className="home-section-img mt-3 me-3" src={Animals} alt="Logo Latidos&Ronrons" />
     </section>
