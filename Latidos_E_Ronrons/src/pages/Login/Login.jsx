41b56a02065cc5989bc2f78521fd0a9e08b4fe80
Luiz Carlos <lcarlos.mln@gmail.com>
2025-07-07 18:29:20 -0300

    ajustes

--- Latidos_E_Ronrons/src/pages/Login/Login.jsx
+++ Latidos_E_Ronrons/src/pages/Login/Login.jsx
@@ -3,12 +3,10 @@ import DogAdocao from '../../assets/PhotoGallery/varios-cachorros.jpg'
 import Navbar from '../../components/Navbar/Navbar';
 import {useForm} from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
-import animal_adoption_mock from '../../data/animal_adoption_mock.js';
 
 function Login() {
 
     const navigate = useNavigate();
-    const randomNumber = Math.floor(Math.random() * 10) + 1; // Gera um número aleatório de 1 a 10
 
     const{
         register, 
@@ -20,26 +18,20 @@ function Login() {
         navigate("/"); //leva o usuário para a página Home
     }
 
-    // Import the mock data
-
-    // Get the photo using randomNumber as the id
-    const animal = animal_adoption_mock.find(a => a.id === randomNumber);
-    const animalPhoto = animal ? animal.photo : DogAdocao;
-
     return (
         <>
         <Navbar />
         <main className="login-main">
             <div className="container-login">
-                <form className="input-fields">
+                <form class="input-fields">
                     <div className="custom-title">
                         <h1>Login</h1>
                         <p>Entre na sua conta</p>
                     </div>
-                    <div className="mb-3 input-login">
-                        <label htmlFor="user-email" className="form-label">Email</label>
+                    <div class="mb-3 input-login">
+                        <label for="user-email" class="form-label">Email</label>
                         <input type="email" 
-                                className="form-control"
+                                class="form-control"
                                 placeholder="Insira seu e-mail" 
                                 {...register('userEmail', {
                                     required: 'E-mail é obrigatório'
@@ -47,10 +39,10 @@ function Login() {
                                 {errors.userEmail && <span>{errors.userEmail.message}</span>}
                     </div>
 
-                    <div className="mb-3 input-login">
-                        <label htmlFor="user-password" className="form-label">Senha</label>
+                    <div class="mb-3 input-login">
+                        <label for="user-password" class="form-label">Senha</label>
                         <input type="password" 
-                                className="form-control"
+                                class="form-control"
                                 placeholder="Insira sua senha" 
                                 {...register('userPassword', {
                                     required: 'Senha é obrigatório'
@@ -60,10 +52,12 @@ function Login() {
                     <button onClick={handleSubmit(onSubmit)} className="btn btn-lg btn-login">Entrar</button>
                 </form>
 
-                <img src={animalPhoto} alt="Animal para adoção" />
+                <img src={DogAdocao} alt="Dog Trovão" />
 
             </div>
         </main>
+       
+      
         </>
     );
 }
