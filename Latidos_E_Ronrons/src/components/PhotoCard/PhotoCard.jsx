e25f2b1410ab00243cfbdd4e0ea9ef230ab445c8
Luiz Carlos <lcarlos.mln@gmail.com>
2025-07-07 16:36:37 -0300

    Add PhotoCard component with styles
    Introduces the PhotoCard component and its CSS for displaying animal photos, names, and descriptions. This component will be used to showcase animals available for adoption.

---
+++ Latidos_E_Ronrons/src/components/PhotoCard/PhotoCard.jsx
@@ -0,0 +1,24 @@
+import "./AnimalCardForAdoption.css";
+import BtnQueroAdotar from "../BtnQueroAdotar/BtnQueroAdotar";
+
+function AnimalCardsForAdoption({ photo_path, animalName, description }) {
+  return (
+    <>
+      <div className="card-gallery-container">
+        <div className="card-gallery mb-3">
+          <img
+            src={photo_path}
+            className="card-img-top"
+            alt={`${animalName}, ${description}`}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{animalName}</h5>
+            <p className="card-text">{description}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AnimalCardsForAdoption;
